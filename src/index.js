cf95737786ada088deb2095a744961572461dc24
Kevin Simper <kevin.simper@gmail.com>
2019-02-28 06:20:08 +0100

    Show region and allow it to be searched

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ class SearchMeetups extends LitElement {
   static get properties() {
     return {
       name: { type: String },
-      communities: { type: Array }
+      communities: { type: Array },
+      countries: { type: Array }
     };
   }
   static get styles() {
@@ -27,15 +28,39 @@ class SearchMeetups extends LitElement {
   constructor() {
     super();
     this.name = "";
-    this.fetching = this.fetchCommunities();
+    this.fetching = Promise.all([
+      this.fetchCommunities(),
+      this.fetchCountries()
+    ]);
   }
   async fetchCommunities() {
     const listRequest = await fetch("./src/list.json");
     const json = await listRequest.json();
     this.communities = json;
   }
+  async fetchCountries() {
+    const listRequest = await fetch("./src/countries-unescaped.json");
+    const json = await listRequest.json();
+    this.countries = json;
+  }
 
   render() {
+    const communitiesResult = this.fetching.then(() => {
+      return this.communities.filter(c => {
+        if (this.name === "") return true;
+        if (
+          c.region &&
+          c.region.toLowerCase().includes(this.name.toLowerCase())
+        ) {
+          return true;
+        }
+        return (
+          c.name.toLowerCase().includes(this.name.toLowerCase()) ||
+          c.city.toLowerCase().includes(this.name.toLowerCase()) ||
+          c.country.toLowerCase().includes(this.name.toLowerCase())
+        );
+      });
+    });
     return html`
       <h4 style="float:right;">
         <a href="https://github.com/kevinsimper/gdg-search"
@@ -43,42 +68,82 @@ class SearchMeetups extends LitElement {
         >
       </h4>
       <h1>🌍 Search all GDG's in the world</h1>
-      <input
-        class="search"
-        type="text"
-        @input="${e => (this.name = e.target.value)}"
-        placeholder="Search.."
-      />
-      <div>
-        ${
-          until(
+      <h3>
+        There is
+        ${until(
+          this.fetching.then(() => {
+            return this.communities.length;
+          }),
+          html`
+            loading..
+          `
+        )} communties</h3>
+        <p>
+          Select a region: ${until(
             this.fetching.then(() => {
-              return this.communities
-                .filter(c => {
-                  if (this.name === "") return true;
+              return [...new Set(this.countries.map(c => c.region))].map(
+                c =>
+                  html`
+                    <a
+                      href="#"
+                      @click="${
+                        e => {
+                          e.preventDefault();
+                          this.name = c;
+                        }
+                      }"
+                      >${c}</a
+                    >
+                  `
+              );
+            })
+          )}
+        </p>
+        <input
+          class="search"
+          type="text"
+          .value=${this.name}
+          @input="${e => (this.name = e.target.value)}"
+          placeholder="Type and search.."
+        />
+        <p>${until(
+          communitiesResult.then(
+            c =>
+              html`
+                Found <strong>${c.length}</strong> results
+              `
+          )
+        )}</p>
+        <hr/>
+        <div>
+          ${until(
+            communitiesResult.then(communities => {
+              return communities.map(c => {
+                let country = this.countries.find(i => {
                   return (
-                    c.name.toLowerCase().includes(this.name.toLowerCase()) ||
-                    c.city.toLowerCase().includes(this.name.toLowerCase()) ||
-                    c.country.toLowerCase().includes(this.name.toLowerCase())
+                    i.name.common === c.country || i.name.official === c.country
                   );
-                })
-                .map(c => {
-                  return html`
-                    <div class="group">
-                      <div>
-                        <a href="https://meetup.com/${c.urlname}">${c.name}</a>
-                      </div>
-                      <div>${c.city}, ${c.country}</div>
-                    </div>
-                  `;
                 });
+                c.region = (country && country.region) || "";
+                return html`
+                  <div class="group">
+                    <div>
+                      <a href="https://meetup.com/${c.urlname}">${c.name}</a>
+                    </div>
+                    <div>
+                      ${c.city}, ${c.country} ${country && country.flag}
+                    </div>
+                    <div>${country && country.region}</div>
+                  </div>
+                `;
+              });
             }),
             html`
               waiting
             `
-          )
-        }
-      </div>
+          )}
+        </div>
+      </h2>
     `;
   }
 }
