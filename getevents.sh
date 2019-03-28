f5c7c80b7ee47e3ce8927af4170e83427bf3991b
Kevin Simper <kevin.simper@gmail.com>
2019-03-28 17:12:14 +0100

    Update getevents.sh

--- getevents.sh
+++ getevents.sh
@@ -1,1 +1,1 @@
-$ cat src/list.json | jq '.[] | .urlname' | tr -d '"' | xargs -n 1 -I{} echo "http https://api.meetup.com/GDGOslo/events status==past > events/{}.json"
+$ cat src/list.json | jq '.[] | .urlname' | tr -d '"' | xargs -n 1 -I{} echo "http https://api.meetup.com/{}/events status==past > events/{}.json"
