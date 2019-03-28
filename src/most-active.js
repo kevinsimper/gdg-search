6846bcbbcc9dfd7fd2ab1a05d318cf833f325b3b
Kevin Simper <kevin.simper@gmail.com>
2019-03-28 01:45:05 +0100

    Show the nice name in the list

--- src/most-active.js
+++ src/most-active.js
@@ -4,12 +4,13 @@ import { until } from "lit-html/directives/until.js";
 class MostActive extends LitElement {
   static get properties() {
     return {
+      active: { type: Array },
       communities: { type: Array }
     };
   }
   constructor() {
     super();
-    this.fetching = this.fetchList();
+    this.fetching = Promise.all([this.fetchList(), this.fetchCommunities()]);
   }
   static get styles() {
     return css`
@@ -22,21 +23,48 @@ class MostActive extends LitElement {
   async fetchList() {
     const listRequest = await fetch("./src/most-active.json");
     const json = await listRequest.json();
+    this.active = json;
+  }
+  async fetchCommunities() {
+    const listRequest = await fetch("./src/list.json");
+    const json = await listRequest.json();
     this.communities = json;
   }
   render() {
     return html`
       <div class="container">
         <h1>Most Active</h1>
-        <ul>
+        <p>
+          This is the list of GDG that has organized the most events. I think it
+          is interesting and I created this list so that anyone can look at
+          other active GDG's and get inspired by what kind of events that they
+          are running. So it is not a competetion about how can create the most
+          events 😄
+        </p>
+        <ol>
           ${
             until(
               this.fetching.then(f => {
                 return html`
                   ${
-                    this.communities.map(c => {
+                    this.active.map(c => {
+                      const community = this.communities.find(
+                        i => i.urlname === c.name
+                      );
+                      if (!community) {
+                        console.log(c);
+                        return html`
+                          <li>${c.name} - ${c.count}</li>
+                        `;
+                      }
                       return html`
-                        <li>${c.name} - ${c.count}</li>
+                        <li>
+                          <a
+                            target="_blank"
+                            href="https://meetup.com/${community.urlname}"
+                            >${community.name} - ${c.count}</a
+                          >
+                        </li>
                       `;
                     })
                   }
@@ -47,7 +75,7 @@ class MostActive extends LitElement {
               `
             )
           }
-        </ul>
+        </ol>
       </div>
     `;
   }
