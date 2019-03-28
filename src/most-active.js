b57c30901619162b7c928951ff69ef78f3302c7e
Kevin Simper <kevin.simper@gmail.com>
2019-03-28 01:24:38 +0100

    List most active communities

--- src/most-active.js
+++ src/most-active.js
@@ -1,6 +1,16 @@
 import { LitElement, css, html } from "lit-element";
+import { until } from "lit-html/directives/until.js";
 
 class MostActive extends LitElement {
+  static get properties() {
+    return {
+      communities: { type: Array }
+    };
+  }
+  constructor() {
+    super();
+    this.fetching = this.fetchList();
+  }
   static get styles() {
     return css`
       .container {
@@ -9,9 +19,36 @@ class MostActive extends LitElement {
       }
     `;
   }
+  async fetchList() {
+    const listRequest = await fetch("./src/most-active.json");
+    const json = await listRequest.json();
+    this.communities = json;
+  }
   render() {
     return html`
-      <div class="container"><h1>Most Active</h1></div>
+      <div class="container">
+        <h1>Most Active</h1>
+        <ul>
+          ${
+            until(
+              this.fetching.then(f => {
+                return html`
+                  ${
+                    this.communities.map(c => {
+                      return html`
+                        <li>${c.name} - ${c.count}</li>
+                      `;
+                    })
+                  }
+                `;
+              }),
+              html`
+                Loading...
+              `
+            )
+          }
+        </ul>
+      </div>
     `;
   }
 }
