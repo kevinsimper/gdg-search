a5c19bcae50ecfd9497e47d3edf6a707eb9182ff
Kevin Simper <kevin.simper@gmail.com>
2019-03-29 16:25:14 +0100

    Calculate yearly and quarterly event counts

--- sumevents.js
+++ sumevents.js
@@ -2,19 +2,23 @@ const { readdirSync, readFileSync, writeFileSync } = require("fs");
 
 const files = readdirSync("./gdg-events").filter(f => !f.includes(".git"));
 
+function rollingperiode(events, months) {
+  const before1year = Date.now() - (365 / 12) * months * 24 * 60 * 60 * 1000;
+  return events.filter(i => i.time > before1year).length;
+}
+
 let meetups = [];
 for (let f in files) {
   const file = "./gdg-events/" + files[f];
-  console.log(file);
   const json = JSON.parse(readFileSync(file));
-  console.log(json.length);
+  const rollingtwelve = rollingperiode(json, 12);
+  const quarter = rollingperiode(json, 3);
   meetups.push({
     name: files[f].replace(".json", ""),
-    count: json.length
+    all: json.length,
+    yearly: rollingtwelve,
+    quarter
   });
 }
 
-meetups.sort((a, b) => {
-  return b.count - a.count;
-});
 writeFileSync("./src/most-active.json", JSON.stringify(meetups, null, 2));
