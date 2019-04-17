9c7d3676605225ec1f8a6a9be8c4390f3a9baac0
Kevin Simper <kevin.simper@gmail.com>
2019-04-18 00:39:48 +0200

    Swtich to download.js

--- getevents.sh
+++ getevents.sh
@@ -1,1 +1,1 @@
-cat src/list.json | jq '.[] | .urlname' | tr -d '"' | xargs -n 1 -I{} echo "http https://api.meetup.com/{}/events status==past > gdg-events/{}.json" > fetch.sh
+cat src/list.json | jq '.[] | .urlname' | tr -d '"' | xargs -n 1 -I{} echo "node -r esm download.js {} > gdg-events/{}.json" > fetch.sh
