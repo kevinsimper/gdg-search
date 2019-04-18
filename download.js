f78a0a82e3b0ba6c61098d5274484c1e58f41127
Kevin Simper <kevin.simper@gmail.com>
2019-04-18 10:34:24 +0200

    Use recursion to download events

--- download.js
+++ download.js
@@ -4,20 +4,32 @@ import fetch from "isomorphic-unfetch";
 const meetup = process.argv[2];
 let events = [];
 async function main() {
-  const res = await fetch(
+  const headerPagination = [
     encodeURI(`https://api.meetup.com/${meetup}/events?status=past`)
-  );
-  const json = await res.json();
-  events.push(json);
-  if (res.headers.get("link")) {
-    const url = res.headers
-      .get("link")
-      .split("<")[1]
-      .split(">")[0];
+  ];
+  async function fetchPage(url) {
     const linkres = await fetch(url);
     const json = await linkres.json();
     events.push(json);
+    // we have fetched all events
+    if (
+      parseInt(linkres.headers.get("X-Total-Count")) === events.flat().length
+    ) {
+      return true;
+    }
+
+    if (linkres.headers.get("link")) {
+      const url = linkres.headers
+        .get("link")
+        .split("<")[1]
+        .split(">")[0];
+      headerPagination.push(url);
+    }
+    if (headerPagination.length > 0) {
+      await fetchPage(headerPagination.pop());
+    }
   }
+  await fetchPage(headerPagination.pop());
   console.log(JSON.stringify(events.flat()));
 }
 
