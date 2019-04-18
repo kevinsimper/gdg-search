eda041b23997daacb2dc27551b758b65bea6b3f6
Kevin Simper <kevin.simper@gmail.com>
2019-04-18 11:50:45 +0200

    Show organizers on the page

--- src/community.js
+++ src/community.js
@@ -1,28 +1,86 @@
 import { LitElement, html, css } from "lit-element";
 import "./components/container.js";
 import "./components/table.js";
-import { fetchEvents } from "./models/index.js";
+import {
+  fetchEvents,
+  fetchOrganizers,
+  fetchCommunities
+} from "./models/index.js";
 
 class Group extends LitElement {
   static get properties() {
     return {
       name: { type: String },
-      events: { type: Array }
+      events: { type: Array },
+      community: { type: Object },
+      communities: { type: Array },
+      organizers: { type: Array }
     };
+  }
+  static get styles() {
+    return css`
+      .organizers {
+        display: flex;
+        margin: 0 0 20px;
+      }
+      .organizer {
+        padding: 0 10px;
+      }
+      .organizer-image {
+        width: 50px;
+        height: 50px;
+        overflow: hidden;
+        border-radius: 100%;
+        display: flex;
+        vertical-align: -15px;
+        margin: 0 6px 0 0;
+      }
+      .organizer img {
+        width: 100%;
+      }
+    `;
   }
   constructor() {
     super();
     this.events = [];
+    this.organizers = [];
+    this.community = {};
+    this.communities = [];
   }
   async connectedCallback() {
     super.connectedCallback();
     const data = await fetchEvents(this.name);
     this.events = data.reverse();
+
+    this.organizers = await fetchOrganizers(this.name);
+
+    await fetchCommunities.bind(this)();
+    this.community = this.communities.find(c => c.urlname === this.name);
   }
   render() {
     return html`
       <x-container>
-        <h1>Community: ${this.name}</h1>
+        <h1>${this.community.name || this.name} - community</h1>
+        <h3>Organizers</h3>
+        <div class="organizers">
+          ${
+            this.organizers.map(o => {
+              return html`
+                <div class="organizer">
+                  ${
+                    o.photo &&
+                      html`
+                        <div class="organizer-image">
+                          <img src="${o.photo.photo_link}" />
+                        </div>
+                      `
+                  }
+                  ${o.name}
+                </div>
+              `;
+            })
+          }
+        </div>
         <x-table
           .content="${
             html`
