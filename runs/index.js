027957c9d0c148a7cbf06d414e690fec69842aed
Kevin Simper <kevin.simper@gmail.com>
2019-07-04 01:05:04 +0200

    Fetch data from github

--- runs/index.js
+++ runs/index.js
@@ -1,7 +1,22 @@
-const { createServer } = require("http");
+const { createServer, get } = require("http");
+const fetch = require("node-fetch");
+
+async function fetchCommunities() {
+  const req = await fetch(
+    "https://raw.githubusercontent.com/kevinsimper/gdg-search/master/src/list.json"
+  );
+  const data = await req.text();
+  return data;
+}
 
 const PORT = process.env.PORT || 3000;
-createServer((req, res) => {
+createServer(async (req, res) => {
   console.log("New request");
-  res.end("Hello GDG Search");
+  if (req.url === "/communities") {
+    const data = await fetchCommunities();
+    res.setHeader("Content-Type", "application/json;");
+    res.end(data);
+  } else {
+    res.end("Hello GDG Search");
+  }
 }).listen(PORT, () => console.log("Listening on http://localhost:" + PORT));
