e379caf66105ea3044cca60f5a896492f4c51af7
Kevin Simper <kevin.simper@gmail.com>
2019-07-04 01:19:31 +0200

    Fetch events from github

--- runs/index.js
+++ runs/index.js
@@ -9,6 +9,19 @@ async function fetchCommunities() {
   return data;
 }
 
+async function fetchEvents(name) {
+  try {
+    const req = await fetch(
+      `https://raw.githubusercontent.com/kevinsimper/gdg-events/master/${name}.json`
+    );
+    const json = await req.json();
+    return json.events;
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+}
+
 const PORT = process.env.PORT || 3000;
 createServer(async (req, res) => {
   console.log("New request");
@@ -16,6 +29,11 @@ createServer(async (req, res) => {
     const data = await fetchCommunities();
     res.setHeader("Content-Type", "application/json;");
     res.end(data);
+  } else if (req.url.match(/\/communities\/(\S*)\/events/)) {
+    const name = req.url.match(/\/communities\/(\S*)\/events/);
+    const events = await fetchEvents(name[1]);
+    res.setHeader("Content-Type", "application/json;");
+    res.end(JSON.stringify(events));
   } else {
     res.end("Hello GDG Search");
   }
