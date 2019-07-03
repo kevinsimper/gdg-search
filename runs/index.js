28cbd253c577e0cdf261fb62afc931d6818776f5
Kevin Simper <kevin.simper@gmail.com>
2019-07-04 01:44:01 +0200

    search all gdg events

--- runs/index.js
+++ runs/index.js
@@ -2,22 +2,26 @@ const { createServer, get } = require("http");
 const fetch = require("node-fetch");
 
 async function fetchCommunities() {
+  // console.log("fetching communities");
   const req = await fetch(
     "https://raw.githubusercontent.com/kevinsimper/gdg-search/master/src/list.json"
   );
-  const data = await req.text();
+  const data = await req.json();
   return data;
 }
 
 async function fetchEvents(name) {
+  // console.log("fetching events", name);
   try {
     const req = await fetch(
-      `https://raw.githubusercontent.com/kevinsimper/gdg-events/master/${name}.json`
+      encodeURI(
+        `https://raw.githubusercontent.com/kevinsimper/gdg-events/master/${name}.json`
+      )
     );
     const json = await req.json();
     return json.events;
   } catch (e) {
-    console.log(e);
+    console.log("error fetch event", name, e.name);
     return [];
   }
 }
@@ -28,12 +32,36 @@ createServer(async (req, res) => {
   if (req.url === "/communities") {
     const data = await fetchCommunities();
     res.setHeader("Content-Type", "application/json;");
-    res.end(data);
+    res.end(JSON.stringify(data));
   } else if (req.url.match(/\/communities\/(\S*)\/events/)) {
     const name = req.url.match(/\/communities\/(\S*)\/events/);
     const events = await fetchEvents(name[1]);
     res.setHeader("Content-Type", "application/json;");
     res.end(JSON.stringify(events));
+  } else if (req.url.startsWith("/search")) {
+    const query = require("url")
+      .parse(req.url, true)
+      .query.name.toLowerCase();
+    const communities = await fetchCommunities();
+    const allEvents = await Promise.all(
+      communities.map(async community => {
+        const events = await fetchEvents(community.urlname);
+        let finds = events.filter(e => {
+          if ("name" in e) {
+            if (e.name.toLowerCase().includes(query)) {
+              return true;
+            }
+          }
+          if ("description" in e) {
+            return e.description.toLowerCase().includes(query);
+          }
+        });
+        return finds;
+      })
+    );
+
+    res.setHeader("Content-Type", "application/json;");
+    res.end(JSON.stringify(allEvents.flat().length));
   } else {
     res.end("Hello GDG Search");
   }
