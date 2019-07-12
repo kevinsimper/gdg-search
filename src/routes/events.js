8bb85fb8309cedae45b52e4da8149cccf912580d
Kevin Simper <kevin.simper@gmail.com>
2019-07-12 23:41:13 +0200

    Query countries page with input field

--- src/routes/events.js
+++ src/routes/events.js
@@ -5,11 +5,13 @@ import "../components/container.js";
 class Events extends LitElement {
   static get properties() {
     return {
-      countries: { type: Array }
+      countries: { type: Array },
+      query: { type: String }
     };
   }
   constructor() {
     super();
+    this.query = "";
     this.fetching = Promise.all([this.fetchCountries()]);
   }
   async fetchCountries() {
@@ -22,33 +24,52 @@ class Events extends LitElement {
       <x-container>
         <h1>Countries</h1>
         <p>This page will show events per country.</p>
-        ${
-          until(
-            this.fetching.then(() => {
-              return html`
-                <table>
-                  ${
-                    this.countries
-                      .map(c => c.name.common)
-                      .sort()
-                      .map(c => {
-                        return html`
-                          <tr>
-                            <td>
-                              <a href="#!events/${c.toLowerCase()}">${c}</a>
-                            </td>
-                          </tr>
-                        `;
-                      })
-                  }
-                </table>
-              `;
-            }),
-            html`
-              Loading
-            `
-          )
-        }
+        <div style="margin: 5px 0">
+          <input
+            type="text"
+            style="font-size: 1.5rem; padding: 5px;"
+            placeholder="Search countries.."
+            @keyup="${e => {
+              this.query = e.target.value;
+            }}"
+          />
+        </div>
+        ${until(
+          this.fetching.then(() => {
+            const content = this.countries
+              .map(c => c.name.common)
+              .filter(
+                c => c.toLowerCase().indexOf(this.query.toLowerCase()) > -1
+              )
+              .sort()
+              .map(c => {
+                return html`
+                  <tr>
+                    <td>
+                      <a href="#!events/${c.toLowerCase()}">${c}</a>
+                    </td>
+                  </tr>
+                `;
+              });
+            return html`
+              <x-table
+                .content="${html`
+                  <thead>
+                    <tr>
+                      <th>Country</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    ${content}
+                  </tbody>
+                `}"
+              ></x-table>
+            `;
+          }),
+          html`
+            Loading
+          `
+        )}
       </x-container>
     `;
   }
