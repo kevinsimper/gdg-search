b3ee30a910ad8bd6267a66e107b70c2df03e60af
Kevin Simper <kevin.simper@gmail.com>
2019-11-24 21:32:59 +0100

    Refactor model into file

--- runs/index.js
+++ runs/index.js
@@ -3,41 +3,12 @@ const express = require("express");
 const fetch = require("node-fetch");
 const pMemoize = require("p-memoize");
 const { ApolloServer, gql } = require("apollo-server-express");
+import {
+  fetchCommunities,
+  fetchEventsOrganizers
+} from "./src/models/community";
 
 const app = express();
-
-async function _fetchCommunities() {
-  // console.log("fetching communities");
-  const req = await fetch(
-    "https://raw.githubusercontent.com/kevinsimper/gdg-search/master/src/list.json"
-  );
-  const data = await req.json();
-  return data;
-}
-const fetchCommunities = pMemoize(_fetchCommunities, { maxAge: 1000 * 3600 });
-
-async function _fetchEventsOrganizers(name) {
-  // console.log("fetching events", name);
-  try {
-    const req = await fetch(
-      encodeURI(
-        `https://raw.githubusercontent.com/kevinsimper/gdg-events/master/${name}.json`
-      )
-    );
-    const json = await req.json();
-    return json;
-  } catch (e) {
-    console.log("error fetch event", name, e.name);
-    return {
-      events: [],
-      organizers: []
-    };
-  }
-}
-
-const fetchEventsOrganizers = pMemoize(_fetchEventsOrganizers, {
-  maxAge: 1000 * 3600
-});
 
 const typeDefs = gql`
   type Organizer {
