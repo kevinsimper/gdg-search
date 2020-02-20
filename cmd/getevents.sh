ac46cf070248bf60410c7aeb04022775efca9fd1
Kevin Simper <kevin.simper@gmail.com>
2020-02-20 18:46:25 +0100

    Update paths

--- cmd/getevents.sh
+++ cmd/getevents.sh
@@ -1,1 +1,1 @@
-cat src/list.json | jq '.[] | .urlname' | tr -d '"' | xargs -n 1 -I{} echo "echo {} && node -r esm download.js {} > gdg-events/{}.json" > fetch.sh
+cat ./src/list.json | jq '.[] | .urlname' | tr -d '"' | xargs -n 1 -I{} echo "echo {} && node -r esm ./cmd/download.js {} > gdg-events/{}.json" > fetch.sh
