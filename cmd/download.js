2565a5a2cb2bb8f9be04aedf65e352069b8737cc
Kevin Simper <kevin.simper@gmail.com>
2020-04-07 21:43:46 +0200

    Improve download script to among other respect ratelimit

--- cmd/download.js
+++ cmd/download.js
@@ -1,34 +1,98 @@
 import fetch from "isomorphic-unfetch";
 import { join } from "path";
-import { writeFileSync } from "fs";
+import { writeFileSync, readFileSync } from "fs";
+
+const sleep = milliseconds => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+};
+
+const debugRatelimit = res => {
+  for (let pair of res.headers.entries()) {
+    if (pair[0].includes("x-ratelimit")) {
+      console.log(pair[0] + ": " + pair[1]);
+    }
+  }
+};
+
+async function recoverFailure(res) {
+  const json = await res.json();
+  // wait additional time to be sure
+  let wait = 10 * 1000;
+
+  switch (json.errors[0].code) {
+    case "throttled":
+      console.log("throttled");
+
+      wait += res.headers.get("x-ratelimit-reset") * 1000 + 10000;
+      console.log("waiting ms", wait);
+      await sleep(wait);
+      return true;
+
+    case "blocked":
+      console.log("blocked");
+
+      wait += res.headers.get("x-ratelimit-reset") * 1000 + 10000;
+      console.log("waiting ms", wait);
+      await sleep(wait);
+      return true;
+
+    case "privacy_error":
+      console.log("group is private");
+      return false;
+
+    default:
+      console.log(json);
+      throw new Error("Request did not succeed!");
+  }
+}
+
+async function respectRatelimit(res) {
+  const remaining = parseInt(res.headers.get("x-ratelimit-remaining"));
+  const reset = parseInt(res.headers.get("x-ratelimit-reset"));
+  if (remaining < 3) {
+    const wait = reset * 1000 + 5000;
+    console.log("respecting ratelimit - less than 2 request left");
+    console.log("waiting ms", wait);
+    await sleep(wait);
+  }
+}
+
+function isThereMorePages(headerPagination, res, results) {
+  if (parseInt(res.headers.get("X-Total-Count")) <= results.flat().length + 1) {
+    return true;
+  }
+
+  if (res.headers.get("link")) {
+    const url = res.headers.get("link").match(/^<(.+)>;/);
+    if (url !== null) {
+      headerPagination.push(url[1]);
+    }
+  }
+}
 
 async function meetupFetch(url) {
+  console.log("");
   const results = [];
   const headerPagination = [url];
   async function fetchPage(url) {
     console.log("Fetching", url);
     const linkres = await fetch(url);
-    for (let pair of linkres.headers.entries()) {
-      if (pair[0].includes("x-ratelimit")) {
-        console.log(pair[0] + ": " + pair[1]);
-      }
-    }
-    const json = await linkres.json();
-    results.push(json);
 
-    if (
-      parseInt(linkres.headers.get("X-Total-Count")) <=
-      results.flat().length + 1
-    ) {
-      return true;
+    if (!linkres.ok) {
+      const shouldRetry = await recoverFailure(linkres);
+      if (shouldRetry) {
+        headerPagination.push(url);
+      }
+    } else {
+      // request succeed
+      const json = await linkres.json();
+      results.push(json);
+
+      isThereMorePages(headerPagination, linkres, results);
+      debugRatelimit(linkres);
+      await respectRatelimit(linkres);
     }
 
-    if (linkres.headers.get("link")) {
-      const url = linkres.headers.get("link").match(/^<(.+)>;/);
-      if (url !== null) {
-        headerPagination.push(url[1]);
-      }
-    }
     if (headerPagination.length > 0) {
       await fetchPage(headerPagination.pop());
     }
@@ -54,13 +118,13 @@ async function fetchOrganizers(meetup) {
   return organizers;
 }
 
-function writeFile(name, data) {
-  const filePath = join(__dirname, `../gdg-events/${name}.json`);
+function writeFile(folder, name, data) {
+  const filePath = `${folder}/${name}.json`;
   console.log("Writing file to path", filePath);
   writeFileSync(filePath, data);
 }
 
-async function main(meetup) {
+async function getAndSaveMeetup(saveFolder, meetup) {
   const events = await fetchEvents(meetup, "past");
   const upcoming = await fetchEvents(meetup, "upcoming");
   const organizers = await fetchOrganizers(meetup);
@@ -74,9 +138,28 @@ async function main(meetup) {
     upcoming: upcoming.length,
     organizers: organizers.length
   });
-  writeFile(meetup, data);
+  writeFile(saveFolder, meetup, data);
 }
 
-// console.log(process.argv[2]);
-const meetup = process.argv[2];
-main(meetup);
+if (require.main === module) {
+  async function main() {
+    // console.log(process.argv[2]);
+    const folder = join(__dirname, `../gdg-events`);
+    const meetupArg = process.argv[2];
+    if (meetupArg) {
+      await getAndSaveMeetup(folder, meetupArg);
+    } else {
+      const list = JSON.parse(
+        readFileSync(join(__dirname, "../src/list.json"))
+      );
+      console.log("Communities:", list.length);
+      let processed = 0;
+      for (const meetup of list) {
+        await getAndSaveMeetup(folder, meetup.urlname);
+        processed++;
+        console.log("rocessed", processed, (processed / list.length) * 100);
+      }
+    }
+  }
+  main();
+}
